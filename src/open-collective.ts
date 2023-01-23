43a05dece1af7cebb49bb5d9976500a9b7d47139
Oliver Ni <oliver.ni@gmail.com>
2023-01-22 20:51:33 -0800

    Include transactions from organizations

--- src/open-collective.ts
+++ src/open-collective.ts
@@ -79,25 +79,32 @@ export interface Metadata {
   is_backer: number;
 }
 
+interface OpenCollectiveMetadataQueryResultAccount {
+  memberOf: {
+    nodes: {
+      totalDonations: {
+        value: number;
+      };
+      account: {};
+      role: string;
+    }[];
+  };
+  transactions: {
+    nodes: {
+      amountInHostCurrency: {
+        value: number;
+      };
+      createdAt: string;
+    }[];
+  };
+}
+
 interface OpenCollectiveMetadataQueryResult {
   data: {
-    account: {
-      memberOf: {
+    account: OpenCollectiveMetadataQueryResultAccount & {
+      organizations: {
         nodes: {
-          totalDonations: {
-            value: number;
-          };
-          role: string;
-        }[];
-      };
-    };
-    collective: {
-      transactions: {
-        nodes: {
-          amountInHostCurrency: {
-            value: number;
-          };
-          createdAt: string;
+          account: OpenCollectiveMetadataQueryResultAccount;
         }[];
       };
     };
@@ -116,24 +123,32 @@ export async function getOpenCollectiveMetadata(
     },
     body: JSON.stringify({
       query: String.raw`
-        query metadata($slug: String, $account_id: String) {
-          account (id: $account_id) {
-            memberOf (account: {slug: $slug}, limit: 1) {
-              nodes {
-                totalDonations {
-                  value
-                }
-                role
+        fragment AccountParts on Account {
+          memberOf (account: {slug: $slug}, limit: 1) {
+            nodes {
+              totalDonations {
+                value
               }
+              role
             }
           }
-          collective(slug: $slug) {
-            transactions(fromAccount: {id: $account_id}, limit: 1, type: CREDIT) {
+          transactions(fromAccount: {slug: $slug}, limit: 1, type: DEBIT) {
+            nodes {
+              amountInHostCurrency {
+                value
+              }
+              createdAt
+            }
+          }
+        }
+        query metadata($slug: String, $account_id: String) {
+          account (id: $account_id) {
+            ...AccountParts
+            organizations: memberOf (accountType: ORGANIZATION) {
               nodes {
-                amountInHostCurrency {
-                  value
+                account {
+                  ...AccountParts
                 }
-                createdAt
               }
             }
           }
@@ -146,21 +161,30 @@ export async function getOpenCollectiveMetadata(
   });
   const json: OpenCollectiveMetadataQueryResult = await res.json();
   let metadata: Metadata = { is_backer: 0 };
-  if (json.data.account.memberOf.nodes.length > 0) {
-    const node = json.data.account.memberOf.nodes[0];
-    metadata.total_donated = Math.ceil(node.totalDonations.value);
+  const accounts = [json.data, ...json.data.account.organizations.nodes];
+  for (const { account } of accounts) {
+    if (account.memberOf.nodes.length == 0) {
+      continue;
+    }
+    const node = account.memberOf.nodes[0];
+    metadata.total_donated ??= 0;
+    metadata.total_donated += Math.ceil(node.totalDonations.value);
     metadata.is_backer =
+      metadata.is_backer ||
       node.role === "BACKER" ||
       node.role === "ADMIN" ||
       node.role === "CONTRIBUTOR" ||
       node.role === "MEMBER"
         ? 1
         : 0;
-  }
-  if (json.data.collective.transactions.nodes.length > 0) {
-    const node = json.data.collective.transactions.nodes[0];
-    metadata.last_donation = node.createdAt;
-    metadata.last_donation_amount = Math.ceil(node.amountInHostCurrency.value);
+    if (account.transactions.nodes.length == 0) {
+      continue;
+    }
+    const transaction = account.transactions.nodes[0];
+    if (!metadata.last_donation || transaction.createdAt > metadata.last_donation) {
+      metadata.last_donation = transaction.createdAt;
+      metadata.last_donation_amount = Math.ceil(-transaction.amountInHostCurrency.value);
+    }
   }
   return metadata;
 }
