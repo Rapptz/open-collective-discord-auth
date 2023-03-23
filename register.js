61ed126b388b57bc52517ff4ca058e010a95db5e
STACIA <69411527+staciax@users.noreply.github.com>
2023-03-23 17:24:21 +0700

    add thai i10n

--- register.js
+++ register.js
@@ -20,10 +20,12 @@ async function run() {
       name: "Total Donated",
       name_localizations: {
         de: "Insgesamt gespendet",
+        th: "ยอดบริจาคทั้งหมด",
       },
       description: "Minimum amount to donate",
       description_localizations: {
         de: "Mindestbetrag, der gespendet werden muss",
+        th: "จำนวนเงินขั้นต่ำที่จะบริจาค",
       },
       type: 2,
     },
@@ -32,10 +34,12 @@ async function run() {
       name: "Last Donation",
       name_localizations: {
         de: "Letzte Spende",
+        th: "การบริจาคครั้งล่าสุด",
       },
       description: "Maximum days since their last donation",
       description_localizations: {
         de: "Maximale Anzahl der Tage seit der letzten Spende",
+        th: "จำนวนวันที่ผ่านไปตั้งแต่การบริจาคครั้งล่าสุด",
       },
       type: 5,
     },
@@ -44,10 +48,12 @@ async function run() {
       name: "Last Donation Amount",
       name_localizations: {
         de: "Letzter Spendenbetrag",
+        th: "จำนวนเงินที่บริจาคครั้งล่าสุด",
       },
       description: "Minimum amount of their last donation",
       description_localizations: {
         de: "Mindestbetrag der letzten Spende",
+        th: "จำนวนเงินขั้นต่ำของการบริจาคครั้งล่าสุด",
       },
       type: 2,
     },
@@ -56,11 +62,13 @@ async function run() {
       name: "Supporter",
       name_localizations: {
         de: "Unterstützer",
+        th: "ผู้สนับสนุน",
       },
       description:
         "The user has either donated at least once before or is a member of the collective",
       description_localizations: {
         de: "Der Benutzer hat entweder mindestens einmal gespendet oder ist Mitglied des Kollektivs",
+        th: "ผู้ใช้เคยบริจาคอย่างน้อยหนึ่งครั้งมาก่อนหรือเป็นสมาชิกของ the collective",
       },
       type: 7,
     },
